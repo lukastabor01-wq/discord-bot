b1499349eb2b5e5008edefe803381a8b2417648d
lukastabor01-wq <lukastabor01@gmail.com>
2026-02-17 21:37:14 +0100

    index.js erstellen

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`✅ Bot online als ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "!ping") {
+    message.reply("pong 🏓");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
